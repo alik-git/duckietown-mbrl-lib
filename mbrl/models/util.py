d5c2b4b25a1cfa83b84aab50c366816b7914bad1
Ali K <ali.kuwajerwala@mail.utoronto.ca>
2021-12-26 16:26:44 -0500

    this fixes a bug for some reason

--- mbrl/models/util.py
+++ mbrl/models/util.py
@@ -153,7 +153,8 @@ class Conv2dEncoder(nn.Module):
         conv = self.convs[0](obs)
         for i in range(1, len(self.convs)):
             conv = self.convs[i](conv)
-        h = conv.view(conv.size(0), -1)
+        # h = conv.view(conv.size(0), -1)
+        h = conv.reshape(conv.size(0), -1)
         return self.fc(h)
 
 
