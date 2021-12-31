275a1db01b67501d3005c6234371dbfefd2013f0
Ali K <ali.kuwajerwala@mail.utoronto.ca>
2021-12-30 22:44:01 -0500

    cleaner

--- mbrl/examples/main.py
+++ mbrl/examples/main.py
@@ -80,5 +80,8 @@ def run(cfg: omegaconf.DictConfig):
 
 if __name__ == "__main__":
 
-    wandb.init(project="<Your W&B Project Name Here>", entity="<Your W&B Username Here>")
+    wandb.init(
+        project="<Your W&B Project Name Here>",
+        entity="<Your W&B Username Here>"
+    )
     run()
