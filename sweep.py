b182d77af3942512ab2a035f525eac046698c7cf
Ali K <alihusein.kuwajerwala@umontreal.ca>
2022-04-25 20:28:00 -0400

    better sweep!

--- sweep.py
+++ sweep.py
@@ -1,18 +1,18 @@
-import argparse
 import os
+import sys
 
-parser = argparse.ArgumentParser(description='Description of your program')
-parser.add_argument('-lr','--model_learning_rate', help='Description for foo argument', type=float, required=True)
-# parser.add_argument('-b','--bar', help='Description for bar argument', required=True)
-args = vars(parser.parse_args())
+base_command = f"python -m mbrl.examples.main algorithm=planet dynamics_model=planet overrides=planet_duckietown algorithm.test_frequency=5 algorithm.num_episodes=15 overrides.sequence_length=80 overrides.batch_size=80"
 
-# cmdd = f"python -m mbrl.examples.main algorithm=planet dynamics_model=planet overrides=planet_duckietown algorithm.test_frequency=2 algorithm.num_episodes=2 overrides.sequence_length=10 overrides.batch_size=10 overrides.model_learning_rate={args['model_learning_rate']}"
+final_hydra_command = base_command
 
-cmdd = f"python -m mbrl.examples.main algorithm=planet dynamics_model=planet overrides=planet_duckietown algorithm.test_frequency=3 algorithm.num_episodes=6 overrides.model_learning_rate={args['model_learning_rate']}"
+for wandb_arg in sys.argv[1:]:
+    hydra_arg = wandb_arg[2:]
+    final_hydra_command += ' ' + f"{hydra_arg}"
 
-print(cmdd)
-os.system(cmdd)
+
+print(final_hydra_command)
+os.system(final_hydra_command)
+
+# Alternate base commands:
 
 # python -m mbrl.examples.main algorithm=planet dynamics_model=planet overrides=planet_duckietown algorithm.test_frequency=2 algorithm.num_episodes=2 overrides.sequence_length=10 overrides.batch_size=10 overrides.model_learning_rate=1e-5
-
-# wandb agent mbrl_ducky/MBRL_Duckyt/e9jiy6fn
