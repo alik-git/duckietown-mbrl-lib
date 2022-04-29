8eca0ba3522ca36e95b1e701b797888f2683eeb7
Ali K <alihusein.kuwajerwala@umontreal.ca>
2022-04-29 01:28:58 -0400

    added commands

--- sweep.py
+++ sweep.py
@@ -16,3 +16,9 @@ os.system(final_hydra_command)
 # Alternate base commands:
 
 # python -m mbrl.examples.main algorithm=planet dynamics_model=planet overrides=planet_duckietown algorithm.test_frequency=2 algorithm.num_episodes=2 overrides.sequence_length=10 overrides.batch_size=10 overrides.model_learning_rate=1e-5
+
+# python -m mbrl.examples.main algorithm=dreamer dynamics_model=dreamer overrides=dreamer_cheetah_run algorithm.test_frequency=2 algorithm.num_episodes=15 overrides.sequence_length=80 overrides.batch_size=50
+
+# python -m mbrl.examples.main algorithm=planet dynamics_model=planet overrides=dreamer_duckietown algorithm.test_frequency=2 algorithm.num_episodes=2 overrides.sequence_length=10 overrides.batch_size=10 overrides.model_learning_rate=1e-4
+
+# python -m mbrl.examples.main algorithm=dreamer dynamics_model=dreamer overrides=dreamer_duckietown algorithm.test_frequency=2 algorithm.num_episodes=1500 overrides.sequence_length=80 overrides.batch_size=50
